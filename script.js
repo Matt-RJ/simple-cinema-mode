a4dd300b85fc83982f951f585593627cffa69036
Mantas Rajackas <35578357+Matt-RJ@users.noreply.github.com>
2019-08-13 20:25:01 +0100

    Update script.js
    Updated to version 1.0 - Basic functionality implemented.

--- script.js
+++ script.js
@@ -1,46 +1,121 @@
 // ==UserScript==
-// @name         Simple Cinema Mode
+// @name         Simple Cinema Mode for YouTube
 // @namespace    https://greasyfork.org/users/313414
-// @version      0.1
-// @description  Easily dim all background elements while watching a video.
+// @version      1.0
+// @description  Dim the background while watching a YouTube video.
 // @author       Matt-RJ
-// @match        *://*/*
-// @grant        GM_registerMenuCommand
-// @grant        unsafeWindow
+// @match        *://*.youtube.com/*
 // ==/UserScript==
 
 (function() {
     'use strict';
 
     var cinemaDiv;
-    createCinemaDiv();
+    var videoContainer = null;
+    var videoContainerOriginalZIndex;
+    var countRenderer = null;
 
-    // Your code here...
-    GM_registerMenuCommand("Cinema Mode", function() {
-        console.log("Cinema mode enabled.");
-        enableCinemaMode();
-    }, 'r');
+    start();
 
-    function enableCinemaMode() {
-        // Create div that covers the entire page, and a selected element (the video) is over this div.
-        document.body.appendChild(cinemaDiv);
-
+    function start() {
+        waitForCountRendererToLoad(function() {
+            waitForVideoToLoad(function() {
+                createCinemaButton();
+            });
+        });
     }
 
-    function createCinemaDiv() {
+    // Ensures that the count renderer (Where the cinema mode button is placed) has loaded.
+    function waitForCountRendererToLoad(callback) {
+        let countRendererCheck = setInterval(checkIfLoaded, 100);
+
+        function checkIfLoaded() {
+            countRenderer = document.getElementById("info-text");
+            if (countRenderer !== null) {
+                clearInterval(countRendererCheck);
+                countRenderer = countRenderer.firstElementChild;
+                callback();
+            }
+        }
+    }
+
+    // Ensures that all the relevant DOM elements for the video are loaded before cinema mode is enabled.
+    function waitForVideoToLoad(callback) {
+        let videoLoadCheck = setInterval(ytdPlayerIsLoaded, 100);
+
+        function ytdPlayerIsLoaded() {
+            let ytdPlayer = document.getElementById("ytd-player");
+            if (ytdPlayer !== null) {
+                clearInterval(videoLoadCheck);
+                videoContainer = ytdPlayer.firstElementChild.firstElementChild;
+                videoContainerOriginalZIndex = videoContainer.style.zIndex;
+                callback();
+            }
+        }
+    }
+
+    function enableCinemaMode() {
+
+        // Creates div that covers the entire page, and a selected element (the video) is pushed in front this div.
         cinemaDiv = document.createElement("div");
+        cinemaDiv.classList.add("cinemaDiv");
         cinemaDiv.style.opacity = "0.95";
         cinemaDiv.style.position = "fixed";
         cinemaDiv.style.width = "100%";
         cinemaDiv.style.height = "100%";
-        cinemaDiv.style.zIndex = "1000";
+        let zIndex = "1000000";
+        cinemaDiv.style.zIndex = zIndex.toString();
         cinemaDiv.style.top = "0px";
         cinemaDiv.style.left = "0px";
         cinemaDiv.style.backgroundColor = "#000000";
 
+        // When clicking outside of the video to go back to normal.
         cinemaDiv.addEventListener("click", function(e) {
             e.target.remove();
-        })
+            // Video div reset to its original z-index once cinema mode is disabled again.
+            videoContainer.style.zIndex = videoContainerOriginalZIndex;
+        });
+
+        document.body.appendChild(cinemaDiv);
+
+        pushToFront(videoContainer);
+    }
+
+    // Returns the highest Z-index out of all the elements on the page.
+    function getHighestZIndexOnPage() {
+        let highestZIndex = Number.MIN_VALUE;
+
+        let allElements = document.getElementsByTagName("*");
+
+        for (let i = 0; i < allElements.length; i++) {
+            let currentZIndex = allElements[i].style.zIndex;
+            if (!isNaN(currentZIndex) && currentZIndex > highestZIndex) {
+                highestZIndex = currentZIndex;
+            }
+        }
+        return highestZIndex;
+    }
+
+    // Sets the z-index of elemToPush to the highest value on the page.
+    function pushToFront(elemToPush) {
+        elemToPush.style.zIndex = getHighestZIndexOnPage() + 1;
+    }
+
+    // Adds a button near the view counter to enable cinema mode.
+    function createCinemaButton() {
+        let button = document.createElement("button");
+        button.type = "button";
+        button.innerHTML = "Cinema Mode";
+        button.style.marginLeft = "10px";
+        button.style.color = "#FFFFFF";
+        button.style.border = "none";
+        button.style.backgroundColor = "#777777";
+
+        button.addEventListener("click", function() {
+            enableCinemaMode();
+        }, false);
+
+        countRenderer.appendChild(button);
     }
 
 })();
